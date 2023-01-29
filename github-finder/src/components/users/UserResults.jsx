56099196289aff4db32e919f03db6e183143d976
Akinwande Tomisin <tomjames156@gmail.com>
2023-01-29 21:07:19 +0100

    added user and loading statesto userresults component

--- github-finder/src/components/users/UserResults.jsx
+++ github-finder/src/components/users/UserResults.jsx
@@ -1,22 +1,33 @@
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 
 function UserResults() {
+    const [users, setUsers] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
+
     useEffect(() => {
         fetchUsers()
     }, [])
 
     const fetchUsers = async ()=> {
-        const response = await fetch(`${process.env.REACT_APP_GITHUB_API_LINK}/users`, {
-            headers: {
-                Authorization: `token github_${process.env.REACT_APP_TOKEN}`
-            }
-        });
+        const response = await fetch(`${process.env.REACT_APP_GITHUB_API_LINK}/users`);
         const data = await response.json()
         console.log(data)
+        setUsers(data)
+        setIsLoading(false)
     }
-  return (
-    <div>user results</div>
-  )
+  if(isLoading){
+    return(
+        <h1>Loading...</h1>
+    )
+  }else{
+    return (
+        <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
+            {users.map((user)=> {
+                return(<h3>{user.login}</h3>)
+        })}
+        </div>
+      )
+  }
 };
 
 export default UserResults;
